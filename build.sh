3e198227b109134aa35666a64360479f014b5b19
Dikshit-Sapkota <dikshitsapkota99@gmail@gmail.com>
2026-09-10 18:53:32 +0200

    Use production virtualenv for deployment

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
+
 set -o errexit
 
-pip install -r requirements.txt
-pnpm --dir frontend install --frozen-lockfile
-pnpm --dir frontend exec tsc -b
-pnpm --dir frontend exec vite build --base=/static/react/ --outDir=../theme/static/react
-python manage.py collectstatic --no-input
+APP_DIR="/home/tap2connectnepal.com/public_html"
+
+source "$APP_DIR/venv/bin/activate"
+
+echo "Python:"
+python --version
+
+echo "Pip:"
+pip --version
+
+pip install -r "$APP_DIR/requirements.txt"
+
+pnpm --dir "$APP_DIR/frontend" install --frozen-lockfile
+
+pnpm --dir "$APP_DIR/frontend" exec tsc -b
+
+pnpm --dir "$APP_DIR/frontend" exec vite build --base=/static/react/ --outDir=../theme/static/react
+
+python "$APP_DIR/manage.py" collectstatic --no-input
